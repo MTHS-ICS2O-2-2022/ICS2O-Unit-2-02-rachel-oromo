2dedd6b527d850402dabdd8fe7a9fbd886675442
rachel-oromo <124389469+rachel-oromo@users.noreply.github.com>
2023-05-04 16:23:09 +0000

    save changes

--- js/script.js
+++ js/script.js
@@ -5,12 +5,11 @@
 // This file contains the JS functions for index.html
 
 /**
- * Do basic math.
+ * Find Area and Perimeter
  */
-function doMathClicked() {
-  document.getElementById("add-math").innerHTML = "<p>6 + 5 = " + (6 + 5) + "</p>"
-  document.getElementById("subtract-math").innerHTML = "<p>7 - 3 =  " + (7 - 3) + "</p>"
-  document.getElementById("multiply-math").innerHTML = "<p>3 + 4 × 2 = " + (3 + 4 * 2) + "</p>"
-  document.getElementById("divide-math").innerHTML = "<p>(4 ÷ 2) + 3 = " + (4 / 2 + 3) + "</p>"
-  document.getElementById("exponent-math").innerHTML = "<p>5 + 2³ = " + (5 + 2 ** 3) + "</p>"
+function calculateClicked() {
+  document.getElementById("area").innerHTML =
+    "<p>The Area is: " + 3 * 5 + " cm²" + "</p>"
+  document.getElementById("perimeter").innerHTML =
+    "<p>The Perimeter is: " + 2 * (3 + 5) + " cm" + "</p>"
 }
